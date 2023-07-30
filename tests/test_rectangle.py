0e84b6d2ea8120270af895f0c681a3495afd1c0a
lspvtv <ringeko1@gmail.com>
2023-07-30 12:23:12 +0300

    final update rectangle test

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -4,13 +4,28 @@ from src.Rectangle import Rectangle
 
 @pytest.mark.parametrize('side_a, side_b, perimetr, area',
                          [
+                             (4, 5, 18, 20),
+                             (1 * 10 ** 31, 9 * 10 ** 31, 2 * 10 ** 32, 9 * 10 ** 62),
+                             (0.1, 0.3, 0.8, 0.03),
+                             (1 / 6, 5 / 6, 2, 5 / 36)
+                         ])
+def test_correct_rectangle(side_a, side_b, perimetr, area):
+    r = Rectangle(side_a, side_b)
+    assert r.name == 'Rectangle'
+    assert r.get_area() == area
+    assert r.get_perimetr() == perimetr
+
+
+@pytest.mark.parametrize('side_a, side_b, perimetr, area',
+                         [
                              (-4, 5, 2, -20),
                              (0, 20, 40, 0),
                              (0, 0, 0, 0),
                              (-2, -3, -10, -6),
+                             (-0.1, -0.3, -0.8, -0.03),
+                             (-1 / 6, -5 / 6, -2, -5 / 36),
                              (4, 4, 16, 16),
                              (10 ** 65, 10 ** 27, 11 * 10 ** 26, 10 ** 52),
-                             (10 ** 15, 10 ** 65, 11 * 10 ** 26, 10 ** 52),
                              ('5', '6', '22', '30')
                          ])
 def test_rectangle_creation_error(side_a, side_b, perimetr, area):
@@ -20,7 +35,8 @@ def test_rectangle_creation_error(side_a, side_b, perimetr, area):
 
 @pytest.mark.parametrize('side_a, side_b, perimetr, area',
                          [
-                             (1 * 10 ** 32, 9 * 10 ** 32, 10 ** 33, 9 * 10 ** 64)
+                             (1 * 10 ** 32, 9 * 10 ** 32, 2 * 10 ** 33, 9 * 10 ** 64)
+
                          ])
 def test_get_area_error(side_a, side_b, perimetr, area):
     r = Rectangle(side_a, side_b)
