ecc7ac2c909097951f16779482e9effad2b55ae6
lspvtv <ringeko1@gmail.com>
2023-08-11 19:22:09 +0300

    correct validation.py

--- src/validation.py
+++ src/validation.py
@@ -1,8 +1,8 @@
-class Validation:
-    def __init__(self, **kwargs):
-        if type(kwargs) == str:
+def validation(*args):
+    for item in args:
+        if type(item) == str:
             raise ValueError("TypeError")
-        if kwargs <= 0:
+        if item <= 0:
             raise ValueError("Can not create")
-        if kwargs > 10 ** 64:
+        if item > 10 ** 64:
             raise ValueError("It is too large, stack overflow")
