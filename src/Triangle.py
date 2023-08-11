211dcf45701cec9196f64cbbeccd50150fbea237
lspvtv <ringeko1@gmail.com>
2023-08-11 20:20:24 +0300

    correction triangle

--- src/Triangle.py
+++ src/Triangle.py
@@ -1,5 +1,6 @@
 import math
 from math import sin
+from src.validation import validation
 
 
 class Triangle:
@@ -8,15 +9,6 @@ class Triangle:
             raise ValueError("This triangle does not exist")
         if (angle_a >= 180 or angle_a <= 0) or (angle_b >= 180 or angle_b <= 0) or (angle_c >= 180 or angle_c <= 0):
             raise ValueError("This triangle does not exist")
-        if type(angle_a) == str or type(angle_b) == str or type(angle_c) == str:
-            raise ValueError("TypeError angle")
-        if type(side_a) == str or type(side_b) == str or type(side_c) == str:
-            raise ValueError("TypeError side")
-        if side_a <= 0 or side_b <= 0 or side_c <= 0:
-            raise ValueError("Can not create Triangle")
-        if (side_a > 10 ** 64 or side_b > 10 ** 64 or side_c > 10 ** 64) or (
-                angle_a > 10 ** 64 or angle_b > 10 ** 64 or angle_c > 10 ** 64):
-            raise ValueError("It is too large, stack overflow")
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
@@ -24,15 +16,14 @@ class Triangle:
         self.angle_b = angle_b
         self.angle_c = angle_c
         self.name = 'Triangle'
+        validation(self.side_a, self.side_b, self.side_c, self.angle_a, self.angle_b, self.angle_c)
 
     def get_area(self):
         area = round(0.5 * self.side_a * self.side_c * (sin(math.radians(self.angle_b)) * 180 / math.pi), 2)
-        if area >= 10 ** 64:
-            raise ValueError("It is too large, stack overflow")
+        validation(area)
         return area
 
     def get_perimetr(self):
         perimetr = round(self.side_a + self.side_b + self.side_c, 2)
-        if perimetr >= 10 ** 64:
-            raise ValueError("It is too large, stack overflow")
+        validation(perimetr)
         return perimetr
